fbea1e05cf3934257276ec9542d8bacd9dfe018b
leegeunhyeok <dev.ghlee@gmail.com>
2024-05-06 22:35:16 +0900

    chore: update demo code

--- demo/basic.ts
+++ demo/basic.ts
@@ -4,6 +4,11 @@ import type { ProgressBar } from '../src/progress-bar';
 
 const TOTAL = 100;
 
+function delay(ms: number): Promise<void> {
+  // eslint-disable-next-line no-promise-executor-return -- demo
+  return new Promise<void>((resolve) => setTimeout(resolve, ms));
+}
+
 function startTask(progress: ProgressBar, stopOnEnd = false): Promise<void> {
   let value = 0;
 
@@ -28,39 +33,49 @@ function startTask(progress: ProgressBar, stopOnEnd = false): Promise<void> {
 }
 
 async function main(): Promise<void> {
+  console.log('create xlane instance');
   const xlane = xLane({
     progressBarSize: 50,
   });
 
-  console.log('start');
-
+  console.log('add progress bars');
   const p1 = xlane.add(TOTAL);
   const p2 = xlane.add(TOTAL);
   const p3 = xlane.add(TOTAL);
   const p4 = xlane.add(TOTAL);
   const p5 = xlane.add(TOTAL);
 
+  console.log('#1 start');
+
   await Promise.all([
     startTask(p1, true),
-    startTask(p2, true),
+    startTask(p2),
+    startTask(p3, true),
+    startTask(p4),
+    startTask(p5, true),
+  ]);
+
+  console.log('#1 end');
+
+  await delay(1000);
+
+  console.log('#2 start');
+
+  await Promise.all([
+    startTask(p1),
+    startTask(p2),
     startTask(p3),
     startTask(p4),
     startTask(p5),
   ]);
 
-  console.log('end 1');
+  console.log('#2 end');
 
-  await Promise.all([
-    startTask(p1, true),
-    startTask(p2, true),
-    startTask(p3),
-    startTask(p4),
-    startTask(p5),
-  ]);
+  console.log('before cleanup');
 
   xlane.removeAll();
 
-  console.log('end 2');
+  console.log('after cleanup');
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises -- demo
